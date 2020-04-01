a48ea734aba43ab9c4215b2e763fb695678f5091
张晋佩 <302784355@qq.com>
2020-04-01 10:50:36 +0800

    添加 expo 插件使用示例

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import { CellGroup, Cell } from '../component-path';
+import { ScrollView, Image, Text } from 'react-native';
+import { Button, CellGroup, Cell } from '../component-path';
+import * as Contacts from 'expo-contacts';
+import * as ImagePicker from 'expo-image-picker';
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
@@ -11,9 +13,63 @@ export default class HomeScreen extends Component {
     this.props.navigation.navigate(route);
   }
 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedImage: null,
+      firstContact: null,
+    };
+  }
+
+  getContacts = async() => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      if (data.length > 0) {
+        const contact = data;
+        console.log(contact);
+
+        this.setState({
+          firstContact: contact[0]
+        });
+      }
+    } else {
+      alert("需要打开通讯录权限！");
+      return;
+    }
+  }
+
+  openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("需要打开相机权限！");
+      return;
+    } else {
+      let pickerResult = await ImagePicker.launchImageLibraryAsync();
+      console.log(pickerResult);
+      if (!pickerResult.cancelled) {
+        this.setState({
+          selectedImage: {
+            localUri: pickerResult.uri 
+          },
+        });
+      }
+    }
+  }
+
   render() {
     return (
       <ScrollView>
+        <Button type="primary" onPress={() => { this.getContacts(); }}>Contacts</Button>
+        {this.state.firstContact ? <Text>{JSON.stringify(this.state.firstContact)}</Text> : null}
+        <Button type="primary" onPress={() => { this.openImagePickerAsync(); }}>Pick a photo</Button>
+        {this.state.selectedImage ? <Image source={{ uri: this.state.selectedImage.localUri }} style={{ height: 300, width: 300 }} /> : null}
+        
         <CellGroup borderOffset={0}>
           <Cell title="ThemeDemo" link onPress={this.goto.bind(this, 'ThemeDemo')} />
           <Cell title="ButtonDemo" link onPress={this.goto.bind(this, 'ButtonDemo')} />
